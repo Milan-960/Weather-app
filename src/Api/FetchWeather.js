4d025c50dab713c314908cfbc3ae305664639af7
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2021-12-29 11:24:56 +0100

    weather apis has been added and css as well

---
+++ src/Api/FetchWeather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import React from "react";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "21b17ce1518a5be6b1097495053cb5eb";
+
+export const fetchweather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+  return data;
+};
